04013dbbd6f29d65db486d11bf820b8e7764f41d
dylan wang <zywang_dylan@126.com>
2018-07-31 14:01:34 +0800

    added the search function

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,5 +1,15 @@
 class Api::V1::ItemsController < Api::V1::BaseController
   def index
     @items = Item.all
+    if params[:query].present?
+      sql_query = " \
+        items.name @@ :query \
+        OR items.description @@ :query \
+        OR users.wechat_name @@ :query \
+      "
+      @items = Item.joins(:user).where(sql_query, query: "%#{params[:query]}%")
+    else
+      @items = Item.all
+    end
   end
 end
