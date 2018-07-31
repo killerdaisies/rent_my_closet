349207e8357215b4ac262fcbfaee48ae1c4e9891
dylan wang <zywang_dylan@126.com>
2018-07-31 14:29:10 +0800

    add crud in items_controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::ItemsController < Api::V1::BaseController
+  before_action :set_item, only: [ :show, :edit, :update, :destroy ]
   def index
     @items = Item.all
     if params[:query].present?
@@ -11,5 +12,53 @@ class Api::V1::ItemsController < Api::V1::BaseController
     else
       @items = Item.all
     end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      render :show
+    else
+      render_error
+    end
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      render :show, status: :created
+    else
+      render_error
+    end
+  end
+
+  def destroy
+    @item.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price)
+  end
+
+
+  def render_error
+    render json: { errors: @item.errors.full_messages },
+      status: :unprocessable_entity
   end
 end
