ad1ccccbea4a1d24edaa5c6159af18b02b9eacd9
Jackson Huang <jacksonhuang1996@outlook.com>
2018-07-31 12:28:20 +0800

    added user controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  # [...]
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :users
-      resources :items
     end
   end
 end
